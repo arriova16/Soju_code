03007cc6dc7e6690cdb3fbd7b9c3883bba61a2bc
Victoria Arriola <arriova16@gmail.com>
2024-11-12 15:22:15 -0600

    plotting not working

--- Soju_saccade.m
+++ Soju_saccade.m
@@ -4,6 +4,7 @@ tld = 'Z:\UserFolders\ToriArriola\Soju_training\Saccade';
 file_dir = dir(tld);
 file_list = dir(fullfile(tld, '*.rsp'));
 data = struct();
+SetFont('Arial', 12)
 
 %% Loading rsp - temp
 
@@ -25,7 +26,6 @@ for i = 1:length(file_list)
 
     response_table = cell2table(temp_data, 'VariableNames', {'Trial', 'CorrectInterval', 'CorrectAnswer', 'Response'});
     data(ii).ResponseTable = response_table;
-    data(ii).NumTrials = size(response_table, 1);
     ii = ii + 1;
 
 end %sac_fld
@@ -45,8 +45,20 @@ for n = 1:length(data)
         end
 
      data(n).PercentCorrect = floor((sum(correct_first)/ tot_trial) *100);
-    
 
-    end
+    end %tot_trial
 
 end %data
+
+%% Plotting percent correct by day
+
+figure; hold on
+
+
+for i = 1:length(data)
+    num_sess = size(data,2);
+    plot(linspace(0,10), data(i).PercentCorrect,'Color', rgb(198, 40, 40), "LineWidth", 3)
+
+
+end
+
